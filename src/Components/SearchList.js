0dbd09a5a9de8583c157ceb15fbd834aadf4946e
fswebdev <47744494+fswebdev@users.noreply.github.com>
2021-11-22 02:08:35 +0530

    Update SearchList.js

--- src/Components/SearchList.js
+++ src/Components/SearchList.js
@@ -4,15 +4,15 @@ import CandidateList from './CandidateList';
 
 const SearchList = (props) => {
   const [input, setInput] = useState('');
- // const [candidateListDefault, setCandidateListDefault] = useState();
+ const [candidateListDefault, setCandidateListDefault] = useState();
   const [candidateList, setCandidateList] = useState();
 
   const fetchData = async () => {
     return await fetch('https://s3-ap-southeast-1.amazonaws.com/he-public-data/users49b8675.json')
       .then(response => response.json())
       .then(data => {
-         setCandidateList(data) 
-        // setCandidateListDefault(data)
+         setCandidateList(data.Id) 
+        setCandidateListDefault(data.Id)
        });}
 
   const updateInput = async (input) => {
