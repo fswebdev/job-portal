d38cb6c8392c1bd084e4f61b49cfb3d888778b3c
fswebdev <47744494+fswebdev@users.noreply.github.com>
2021-11-22 01:34:48 +0530

    Create SearchList.js

---
+++ src/Components/SearchList.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import SearchBar from './SearchBar';
+import CanfidateList from './CandidateList';
+
+const SearchList = (props) => {
+  const [input, setInput] = useState('');
+  const [candidateListDefault, setCandidateListDefault] = useState();
+  const [candidateList, setCandidateList] = useState();
+
+  const fetchData = async () => {
+    return await fetch('https://s3-ap-southeast-1.amazonaws.com/he-public-data/users49b8675.json')
+      .then(response => response.json())
+      .then(data => {
+         setCandidateList(data) 
+         setCandidateListDefault(data)
+       });}
+
+  const updateInput = async (input) => {
+     const filtered = candidateListDefault.filter(candidate => {
+      return candidate.name.toLowerCase().includes(input.toLowerCase())
+     })
+     setInput(input);
+     setCandidateList(filtered);
+  }
+
+  useEffect( () => {fetchData()},[]);
+	
+  return (
+    <>
+      <h1>Candidate List</h1>
+      <SearchBar 
+       input={input} 
+       onChange={updateInput}
+      />
+      <CandidateList candidateList={candidateList}/>
+    </>
+   );
+}
+
+export default SearchList
